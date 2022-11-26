8c396313f2821eb02944de49a2f6ef4741bde7eb
Edwin Saul <edwinsaulpm@gmail.com>
2022-11-26 08:24:23 -0500

    minimal changes

---
+++ New_index.html.py
@@ -0,0 +1,18 @@
+#make a new index.html file
+
+texto="""<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>    </title>
+  </head>
+  <body>
+
+
+  </body>
+</html>"""
+f=open("index.html","w")
+f.write(texto)
+f.close()
+
+
